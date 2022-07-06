47cf083c61b9b283cf1ed53682b1bf27e81ea904
RodrigoRPG <rodrigoaf777@gmail.com>
2022-07-05 21:18:35 -0300

    update retorno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BancoComponent } from './banco/banco.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BancoComponent
   ],
   imports: [
     BrowserModule,
