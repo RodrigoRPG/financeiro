97abf659eb2b53e620b1cbd6b4bdb1ec4f434fb2
RodrigoRPG <rodrigoaf777@gmail.com>
2022-07-06 22:15:32 -0300

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  saldo = 'O valor do saldo é 0'
-  tempo = 'O tempo não para persistência é necessária'
-  OlaMundo = 'Olá Mundo!'
-  title = 'financeiro';
+export class AppComponent implements OnInit{
+  saldo: any;
+  tempo: any;
+  OlaMundo: any;
+  title: any;
+  Clientes: any [] = ['Fabiana', 'Karen', 'Beatriz', 'Felipe', 'Khristopher']
+  contructor (){
+  this.saldo = 'O valor do saldo é 0';
+  this.tempo = 'O tempo não para persistência é necessária';
+  this.OlaMundo = 'Olá Mundo!';
+  this.title = 'financeiro';
+    for (let i=0; i<this.Clientes.length; i++){
+      let Clientes = this.Clientes[i];
+    }
+}
+  ngOnInit() {
+    
+  }
 }
