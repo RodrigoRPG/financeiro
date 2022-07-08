6ef458abd683ec1b984a91714721c404eb946045
RodrigoRPG <rodrigoaf777@gmail.com>
2022-07-07 23:00:36 -0300

    correção interpolação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  saldo: any;
-  tempo: any;
-  OlaMundo: any;
-  title: any;
+
+
+  saldo = 'O valor do saldo é 0';
+  tempo = 'O tempo não para persistência é necessária';
+  OlaMundo = 'Olá Mundo!';
+  title = 'financeiro';
+  
+
   Clientes: any [] = ['Fabiana', 'Karen', 'Beatriz', 'Felipe', 'Khristopher']
   contructor (){
-  this.saldo = 'O valor do saldo é 0';
-  this.tempo = 'O tempo não para persistência é necessária';
-  this.OlaMundo = 'Olá Mundo!';
-  this.title = 'financeiro';
-    for (let i=0; i<this.Clientes.length; i++){
-      let Clientes = this.Clientes[i];
-    }
+
+   
 }
   ngOnInit() {
+   
     
   }
 }
